868dfe9f43a2a2682fdb3f6f31b09f432fd8dae4
V <countlavegas@gmail.com>
2021-06-18 17:01:54 +0530

    Added a basic handler

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-const App = () => {
-    return React.createElement(
-      "div",
-      {},
-      React.createElement("h1", {}, "Adopt Me!")
-    );
-  };
- ReactDOM.render(React.createElement(App), document.getElementById("root"));
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+class App extends React.Component{
+  handleTitleClick(){
+    alert("You clicked the title");
+  }
+  render(){
+    return React.createElement("div", {}, [
+      React.createElement("h1", { onClick:this.handleTitleClick }, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
+    ]);
+
+  }
+}
+
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
