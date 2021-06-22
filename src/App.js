85aea919641a987c71caee6e545b466f86aa6944
V <countlavegas@gmail.com>
2021-06-22 18:33:50 +0530

    Added JSX to pet.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
-class App extends React.Component{
-  handleTitleClick(){
+
+class App extends React.Component {
+  handleTitleClick() {
     alert("You clicked the title");
   }
-  render(){
+  render() {
     return React.createElement("div", {}, [
-      React.createElement("h1", { onClick:this.handleTitleClick }, "Adopt Me!"),
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Adopt Me!"
+      ),
       React.createElement(Pet, {
         name: "Luna",
         animal: "Dog",
-        breed: "Havanese"
+        breed: "Havanese",
       }),
       React.createElement(Pet, {
         name: "Pepper",
         animal: "Bird",
-        breed: "Cockatiel"
+        breed: "Cockatiel",
       }),
-      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
+      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
     ]);
-
   }
 }
-
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
