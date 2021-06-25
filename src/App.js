cace7456edf5417d2585b60262df02914031f83c
V <countlavegas@gmail.com>
2021-06-25 21:51:28 +0530

    Implemented petfinder API

--- src/App.js
+++ src/App.js
@@ -1,31 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+import pf from "petfinder-client";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      pets:[]
+    };
+  }
   handleTitleClick() {
     alert("You clicked the title");
   }
+  componentDidMount() {
+    petfinder.pet.find({ output: "full", location:"Las vegas,NV" })
+    .then(data =>{
+      let pets;
+
+      if(data.petfinder.pets && data.petfinder.pets.pet){
+        if(Array.isArray(data.petfinder.pets.pet)){
+          pets=data.petfinder.pets.pet;
+        }
+        else{
+          pets=[data.petfinder.pets.pet];
+        }
+      }
+      else{
+        pets=[];
+      }
+      this.setState({
+        pets:pets
+      });
+    })
+  }
+  
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-    ]);
+    return (<div>
+      <h1 onClick={this.handleTitleClick} >Adopt Me!</h1>
+
+      {this.state.pets.map(pet => {
+        let breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+        return (
+          <Pet
+            animal={pet.animal}
+            key={pet.id}
+            name={pet.name}
+            breed={breed}
+            media={pet.media}
+            location={`${pet.contact.city}, ${pet.contact.state}`}
+          />
+        );
+      })}
+    </div>);
+
+    
   }
 }
 
--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-export default function Pet({ name, animal, breed }) {
+class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+    let photos = [];
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
     return (
-      <div>
-        <h1>{name}</h1>
-        <h2>{animal}</h2>
-        <h2>{breed}</h2>
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+        </div>
       </div>
-
     );
   }
+}
+
+export default Pet;
+
